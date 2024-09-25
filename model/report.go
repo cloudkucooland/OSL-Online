d5c5df3cdde6eaef9ec1454959f931d36a6c65f3
Scot Bontrager <cloudkucooland@gmail.com>
2024-09-25 02:50:17 +0000

    add

---
+++ model/report.go
@@ -0,0 +1,127 @@
+package model
+
+import (
+	"database/sql"
+	"log/slog"
+	"time"
+)
+
+func ReportNotRenewed() ([]*Member, error) {
+	var members []*Member
+	var n MemberImport
+
+	var ra sql.NullString
+
+	rows, err := db.Query("SELECT FirstName, LastName, PreferredName, Title, Address, AddressLine2, City, State, Country, PostalCode, PrimaryEmail, DateReaffirmation FROM member WHERE MemberStatus = 'Annual Vows' AND DateReaffirmation < DATE_SUB(CURRENT_DATE(), INTERVAL 365 DAY) ORDER BY DateReaffirmation")
+	if err != nil {
+		slog.Error(err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&n.FirstName, &n.LastName, &n.PreferredName, &n.Title, &n.Address, &n.AddressLine2, &n.City, &n.State, &n.Country, &n.PostalCode, &n.PrimaryEmail, &ra)
+		if err != nil {
+			slog.Error(err.Error())
+			return nil, err
+		}
+
+		if ra.Valid {
+			n.DateReaffirmation, _ = time.Parse("2006-01-02", ra.String)
+		}
+		members = append(members, (&n).toMember())
+	}
+	return members, nil
+}
+
+func ReportExpired() ([]*Member, error) {
+	var members []*Member
+	var n MemberImport
+
+	var ra sql.NullString
+
+	rows, err := db.Query("SELECT FirstName, LastName, PreferredName, Title, Address, AddressLine2, City, State, Country, PostalCode, PrimaryEmail, DateReaffirmation FROM member WHERE MemberStatus = 'Annual Vows' AND DateReaffirmation < DATE_SUB(CURRENT_DATE(), INTERVAL 730 DAY) ORDER BY DateReaffirmation")
+	if err != nil {
+		slog.Error(err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&n.FirstName, &n.LastName, &n.PreferredName, &n.Title, &n.Address, &n.AddressLine2, &n.City, &n.State, &n.Country, &n.PostalCode, &n.PrimaryEmail, &ra)
+		if err != nil {
+			slog.Error(err.Error())
+			return nil, err
+		}
+
+		if ra.Valid {
+			n.DateReaffirmation, _ = time.Parse("2006-01-02", ra.String)
+		}
+		members = append(members, (&n).toMember())
+	}
+	return members, nil
+}
+
+func ReportEmail() ([]*Member, error) {
+	var members []*Member
+	var n MemberImport
+
+	rows, err := db.Query("SELECT MemberStatus, FirstName, LastName, PreferredName, Title, LifevowName, Suffix, PrimaryEmail, SecondaryEmail, ListPrimaryEmail, ListSecondaryEmail, Doxology, Newsletter, Communication FROM member WHERE MemberStatus = 'Annual Vows' OR MemberStatus = 'Life Vows' ORDER BY LastName, FirstName")
+	if err != nil {
+		slog.Error(err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&n.MemberStatus, &n.FirstName, &n.LastName, &n.PreferredName, &n.Title, &n.LifevowName, &n.Suffix, &n.PrimaryEmail, &n.SecondaryEmail, &n.ListPrimaryEmail, &n.ListSecondaryEmail, &n.Doxology, &n.Newsletter, &n.Communication)
+		if err != nil {
+			slog.Error(err.Error())
+			return nil, err
+		}
+
+		members = append(members, (&n).toMember())
+	}
+	return members, nil
+}
+
+func ReportAnnual() ([]*Member, error) {
+	var members []*Member
+	var n MemberImport
+
+	rows, err := db.Query("SELECT FirstName, LastName, PreferredName, Title, Suffix, Address, AddressLine2, City, State, Country, PostalCode, Doxology, Newsletter, Communication FROM member WHERE MemberStatus = 'Annual Vows' ORDER BY LastName, FirstName")
+	if err != nil {
+		slog.Error(err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&n.FirstName, &n.LastName, &n.PreferredName, &n.Title, &n.Suffix, &n.Address, &n.AddressLine2, &n.City, &n.State, &n.Country, &n.PostalCode, &n.Doxology, &n.Newsletter, &n.Communication)
+		if err != nil {
+			slog.Error(err.Error())
+			return nil, err
+		}
+
+		members = append(members, (&n).toMember())
+	}
+	return members, nil
+}
+
+func ReportLife() ([]*Member, error) {
+	var members []*Member
+	var n MemberImport
+
+	rows, err := db.Query("SELECT FirstName, LastName, PreferredName, Title, Suffix, LifevowName, Address, AddressLine2, City, State, Country, PostalCode, Doxology, Newsletter, Communication FROM member WHERE MemberStatus = 'Life Vows' ORDER BY LastName, FirstName")
+	if err != nil {
+		slog.Error(err.Error())
+		return nil, err
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&n.FirstName, &n.LastName, &n.PreferredName, &n.Title, &n.Suffix, &n.LifevowName, &n.Address, &n.AddressLine2, &n.City, &n.State, &n.Country, &n.PostalCode, &n.Doxology, &n.Newsletter, &n.Communication)
+		if err != nil {
+			slog.Error(err.Error())
+			return nil, err
+		}
+
+		members = append(members, (&n).toMember())
+	}
+	return members, nil
+}
---
+++ rest/reports.go
@@ -0,0 +1,196 @@
+package rest
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/cloudkucooland/OSL-Online/model"
+	"github.com/julienschmidt/httprouter"
+)
+
+func reportNotrenewed(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	out := [][]string{
+		{"DateReaffirmation", "FirstName", "LastName", "PreferredName", "Title", "Address", "AddressLine2", "City", "State", "Country", "PostalCode", "PrimaryEmail"},
+	}
+	headers(w, r)
+
+	m, err := model.ReportNotRenewed()
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	for _, n := range m {
+		y, m, d := n.DateReaffirmation.Date()
+		s := fmt.Sprintf("%04d-%02d-%02d", y, m, d)
+		member := []string{s, n.FirstName, n.LastName, n.PreferredName, n.Title, n.Address, n.AddressLine2, n.City, n.State, n.Country, n.PostalCode, n.PrimaryEmail}
+		out = append(out, member)
+	}
+
+	w.Header().Set("Content-Type", "text/csv")
+	report := csv.NewWriter(w)
+	report.WriteAll(out)
+
+	if err := report.Error(); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func reportExpired(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	out := [][]string{
+		{"DateReaffirmation", "FirstName", "LastName", "PreferredName", "Title", "Address", "AddressLine2", "City", "State", "Country", "PostalCode", "PrimaryEmail"},
+	}
+	headers(w, r)
+
+	m, err := model.ReportExpired()
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	for _, n := range m {
+		y, m, d := n.DateReaffirmation.Date()
+		s := fmt.Sprintf("%04d-%02d-%02d", y, m, d)
+		member := []string{s, n.FirstName, n.LastName, n.PreferredName, n.Title, n.Address, n.AddressLine2, n.City, n.State, n.Country, n.PostalCode, n.PrimaryEmail}
+		out = append(out, member)
+	}
+
+	w.Header().Set("Content-Type", "text/csv")
+	report := csv.NewWriter(w)
+	report.WriteAll(out)
+
+	if err := report.Error(); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func reportEmail(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	out := [][]string{
+		{"MemberStatus", "FirstName", "LastName", "PreferredName", "Title", "LifevowName", "Suffix", "PrimaryEmail", "SecondaryEmail", "ListPrimaryEmail", "ListSecondaryEmail", "Doxology", "Newsletter", "Communication"},
+	}
+	headers(w, r)
+
+	m, err := model.ReportEmail()
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	for _, n := range m {
+		member := []string{n.MemberStatus, n.FirstName, n.LastName, n.PreferredName, n.Title, n.LifevowName, n.Suffix, n.PrimaryEmail, n.SecondaryEmail, fmt.Sprintf("%t", n.ListPrimaryEmail), fmt.Sprintf("%t", n.ListSecondaryEmail), n.Doxology, n.Newsletter, n.Communication}
+		out = append(out, member)
+	}
+
+	w.Header().Set("Content-Type", "text/csv")
+	report := csv.NewWriter(w)
+	report.WriteAll(out)
+
+	if err := report.Error(); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func reportAnnual(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	out := [][]string{
+		{"PostalName", "FirstName", "LastName", "PreferredName", "Title", "Suffix", "Address", "AddressLine2", "City", "State", "Country", "PostalCode", "Doxology", "Newsletter", "Communication"},
+	}
+	headers(w, r)
+
+	m, err := model.ReportAnnual()
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	for _, n := range m {
+		firstname := false
+		postalName := n.Title + " "
+		if n.PreferredName != "" {
+			postalName += n.PreferredName
+			firstname = true
+		}
+		if !firstname && n.FirstName != "" {
+			postalName += n.FirstName
+			firstname = true
+		}
+		postalName += " " + n.LastName
+		if n.Suffix != "" {
+			postalName += " " + n.Suffix
+		}
+		postalName += ", OSL"
+
+		member := []string{postalName, n.FirstName, n.LastName, n.PreferredName, n.Title, n.Suffix, n.Address, n.AddressLine2, n.City, n.State, n.Country, n.PostalCode, n.Doxology, n.Newsletter, n.Communication}
+		out = append(out, member)
+	}
+
+	w.Header().Set("Content-Type", "text/csv")
+	report := csv.NewWriter(w)
+	report.WriteAll(out)
+
+	if err := report.Error(); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func reportLife(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	out := [][]string{
+		{"PostalName", "FirstName", "LastName", "PreferredName", "Title", "Suffix", "LifevowName", "Address", "AddressLine2", "City", "State", "Country", "PostalCode", "Doxology", "Newsletter", "Communication"},
+	}
+	headers(w, r)
+
+	m, err := model.ReportLife()
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	for _, n := range m {
+		firstname := false
+		postalName := n.Title + " "
+		if n.LifevowName != "" {
+			postalName += n.LifevowName
+			firstname = true
+		}
+		if !firstname && n.PreferredName != "" {
+			postalName += n.PreferredName
+			firstname = true
+		}
+		if !firstname && n.FirstName != "" {
+			postalName += n.FirstName
+			firstname = true
+		}
+		postalName += " " + n.LastName
+		if n.Suffix != "" {
+			postalName += " " + n.Suffix
+		}
+		postalName += ", OSL"
+
+		member := []string{postalName, n.FirstName, n.LastName, n.PreferredName, n.Title, n.Suffix, n.LifevowName, n.Address, n.AddressLine2, n.City, n.State, n.Country, n.PostalCode, n.Doxology, n.Newsletter, n.Communication}
+		out = append(out, member)
+	}
+
+	w.Header().Set("Content-Type", "text/csv")
+	report := csv.NewWriter(w)
+	report.WriteAll(out)
+
+	if err := report.Error(); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
