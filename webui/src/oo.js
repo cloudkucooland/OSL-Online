fb345c77253e5bd26b9e05d3cf90488c7c4b15aa
Scot Bontrager <cloudkucooland@gmail.com>
2024-09-25 02:49:05 +0000

    add reports, bring up-to-date with server

--- webui/src/oo.js
+++ webui/src/oo.js
@@ -1,4 +1,4 @@
-export const server = "http://saint-luke.net:8443";
+export const server = "https://saint-luke.net:8443";
 
 export function getMe() {
   const jwt = localStorage.getItem('jwt');
@@ -86,61 +86,62 @@ export async function getMember(id) {
 
   // trim unnecessary times
   let sp = payload.DateReaffirmation.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DateReaffirmation = '';
   } else {
     payload.DateReaffirmation = sp[0];
   }
 
   sp = payload.DateFirstProfession.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DateFirstProfession= '';
   } else {
     payload.DateFirstProfession = sp[0];
   }
 
   sp = payload.DateFirstVows.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DateFirstVows= '';
   } else {
     payload.DateFirstVows = sp[0];
   }
   
   sp = payload.DateNovitiate.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DateNovitiate= '';
   } else {
     payload.DateNovitiate = sp[0];
   }
 
   sp = payload.BirthDate.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.BirthDate= '';
   } else {
     payload.BirthDate = sp[0];
   }
 
   sp = payload.DateDeceased.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DateDeceased= '';
   } else {
     payload.DateDeceased = sp[0];
   }
 
   sp = payload.DateRecordCreated.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DateRecordCreated= '';
   } else {
     payload.DateRecordCreated = sp[0];
   }
 
   sp = payload.DateRemoved.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DateRemoved = '';
   } else {
     payload.DateRemoved = sp[0];
   }
 
+  console.log(payload);
   return payload;
 }
 
@@ -224,7 +225,7 @@ export async function getSubscriber(id) {
 
   // trim unnecessary times
   let sp = payload.DatePaid.split('T');
-  if ( sp[0] == "1800-01-01" ) {
+  if ( sp[0] == "0001-01-01" ) {
     payload.DatePaid= '';
   } else {
     payload.DatePaid = sp[0];
@@ -261,3 +262,43 @@ export async function updateSubscriber(id, FieldName, Value) {
   }
 }
 
+export async function report(reportname) {
+  const jwt = localStorage.getItem('jwt');
+  if (jwt === undefined || jwt === null) {
+    throw new Error("Not Logged in");
+  }
+
+  const request = {
+    method: "GET",
+    mode: "cors",
+    credentials: "include",
+    redirect: "manual",
+    referrerPolicy: "origin",
+    headers: {Authorization: "Bearer " + jwt}
+  };
+
+  const response = await fetch(`${server}/api/v1/report/${reportname}`, request);
+  const payload = await response.text();
+  if (response.status != 200) {
+    console.log("server returned ", response.status);
+    throw new Error(payload.error);
+  }
+
+  // https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+  const blob = new Blob([payload], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+    navigator.msSaveBlob(blob, `${reportname}.csv`);
+  } else {
+    const link = document.createElement("a");
+    if (link.download !== undefined) { // feature detection
+        // Browsers that support HTML5 download attribute
+        const url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", `${reportname}.csv`);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+  }
+}
