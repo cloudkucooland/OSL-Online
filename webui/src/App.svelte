70cfeaf7d1a3e143ebce6f9c8e633e92585c1699
Scot Bontrager <cloudkucooland@gmail.com>
2024-10-04 15:32:19 +0000

    format

--- webui/src/App.svelte
+++ webui/src/App.svelte
@@ -1,57 +1,78 @@
 <script>
-import Router from "svelte-spa-router";
-import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Footer } from 'flowbite-svelte';
-import { SvelteToast } from '@zerodevx/svelte-toast';
-import { DarkMode } from 'flowbite-svelte';
+	import { setContext, getContext } from 'svelte';
+	import { writable } from 'svelte/store';
+	import Router from 'svelte-spa-router';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Footer,
+		FooterCopyright,
+		FooterLink
+	} from 'flowbite-svelte';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { DarkMode } from 'flowbite-svelte';
 
-import HomePage from "./routes/HomePage.svelte";
-import Login from "./routes/Login.svelte";
-import Reports from "./routes/Reports.svelte";
-import Member from "./routes/Member.svelte";
-import AddMember from "./routes/AddMember.svelte";
-import SubSearch from "./routes/SubSearch.svelte";
-import Subscriber from "./routes/Subscriber.svelte";
-import AddSubscriber from "./routes/AddSubscriber.svelte";
+	import HomePage from './routes/HomePage.svelte';
+	import Login from './routes/Login.svelte';
+	import Reports from './routes/Reports.svelte';
+	import Member from './routes/Member.svelte';
+	import AddMember from './routes/AddMember.svelte';
+	import SubSearch from './routes/SubSearch.svelte';
+	import Subscriber from './routes/Subscriber.svelte';
+	import AddSubscriber from './routes/AddSubscriber.svelte';
+	import Register from './routes/Register.svelte';
+	import Me from './routes/Me.svelte';
+	import { getMe } from './oo';
 
-/* import "./app.css"; */
+	setContext('oo', { me: writable(getMe()) });
+	const { me } = getContext('oo');
+	// console.log("from context", me);
 
-const routes = {
-  "/": HomePage,
-  "/Search/:query": HomePage,
+	const routes = {
+		'/': HomePage,
+		'/search/:query': HomePage,
 
-  "/login": Login,
-  "/reports": Reports,
+		'/login': Login,
+		'/register': Register,
+		'/reports': Reports,
 
-  "/member/:id": Member,
-  "/addmember/": AddMember,
+		'/me': Me,
+		'/member/:id': Member,
+		'/addmember/': AddMember,
 
-  "/subsearch": SubSearch,
-  "/subscriber/:id": Subscriber,
-  "/addsubscriber/": AddSubscriber,
-  "*": HomePage,
-};</script>
+		'/subsearch': SubSearch,
+		'/subscriber/:id': Subscriber,
+		'/addsubscriber/': AddSubscriber,
+		'*': HomePage
+	};
+</script>
 
-<Navbar >
-  <NavBrand href="#/">
-    <span class="dark:text-white">OSL Member Manager</span>
-  </NavBrand>
-  <NavHamburger></NavHamburger>
-  <NavUl>
-    <NavLi href="#/reports">Reports</NavLi>
-  </NavUl>
-</Navbar>
-<SvelteToast></SvelteToast>
+<svelte:window />
+<header class="w-full flex-none bg-white dark:bg-slate-950">
+	<Navbar>
+		<NavBrand href="#/">
+			<span class="whitspace-nowrap self-center text-xl dark:text-white">OSL Member Directory</span>
+		</NavBrand>
+		<NavHamburger></NavHamburger>
+		<NavUl>
+			<NavLi href="#/me">Me</NavLi>
+			{#if $me && $me.level > 1}
+				<NavLi href="#/reports">Reports</NavLi>{/if}
+		</NavUl>
+	</Navbar>
+	<div class="flex gap-10"></div>
+	<SvelteToast />
+</header>
 
 <main>
-  <Router {routes}></Router>
+	<Router {routes}></Router>
 </main>
 
-<Footer class="sm:px-4 py-2.5 bottom-0 start-0 border-t">
-  <p class="text-muted small text-end">
-  <DarkMode />
-    Copyright © <a href="https://saint-luke.net/">
-      The Order of St. Luke ®
-    </a> 2024. All Rights Reserved. &nbsp;|&nbsp;
-    <a href="https://github.com/cloudkucooland/OSL-Online"> GitHub </a>
-  </p>
+<Footer class="bottom-0 start-0 border-t py-2.5 sm:px-4">
+	<FooterCopyright href="/" by="The Order of St. Luke ®" year={2024} />
+	{#if $me}<FooterLink href="#/Login">Log out</FooterLink>{/if}
+	<DarkMode />
 </Footer>
--- webui/src/oo.js
+++ webui/src/oo.js
@@ -1,343 +1,441 @@
-export const server = "https://saint-luke.net:8443";
+export const server = 'https://saint-luke.net:8443';
 
 export function getMe() {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    return undefined;
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		return undefined;
+	}
 
-  const token = JSON.parse(window.atob(jwt.split(".")[1]).toString());
-  return token;
+	const token = JSON.parse(window.atob(jwt.split('.')[1]).toString());
+	return token;
 }
 
 export async function getJWT(username, password) {
-  const dataArray = new FormData();
-  dataArray.append("username", username);
-  dataArray.append("password", password);
+	const dataArray = new FormData();
+	dataArray.append('username', username);
+	dataArray.append('password', password);
 
-  const request = {
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    body: dataArray,
-  };
+	const request = {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		body: dataArray
+	};
 
-  const response = await fetch(`${server}/api/v1/getJWT`, request);
-  const payload = await response.text();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
-  localStorage.setItem("jwt", payload);
+	const response = await fetch(`${server}/api/v1/getJWT`, request);
+	const payload = await response.text();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
+	localStorage.setItem('jwt', payload);
 }
 
 export async function search(query) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const dataArray = new FormData();
-  dataArray.append("query", query);
+	const dataArray = new FormData();
+	dataArray.append('query', query);
 
-  const request = {
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    body: dataArray,
-    headers: { Authorization: "Bearer " + jwt },
-  };
+	const request = {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		body: dataArray,
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
 
-  const response = await fetch(`${server}/api/v1/search`, request);
-  const payload = await response.json();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
-  console.log(payload);
-  return payload;
+	const response = await fetch(`${server}/api/v1/search`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
+	// console.log(payload);
+	return payload;
 }
 
 export async function getMember(id) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const request = {
-    method: "GET",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    headers: { Authorization: "Bearer " + jwt },
-  };
+	const request = {
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
 
-  const response = await fetch(`${server}/api/v1/member/${id}`, request);
-  const payload = await response.json();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
+	const response = await fetch(`${server}/api/v1/member/${id}`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
 
-  // trim unnecessary times
-  let sp = payload.DateReaffirmation.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DateReaffirmation = "";
-  } else {
-    payload.DateReaffirmation = sp[0];
-  }
+	// trim unnecessary times
+	let sp = payload.DateReaffirmation.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateReaffirmation = '';
+	} else {
+		payload.DateReaffirmation = sp[0];
+	}
 
-  sp = payload.DateFirstProfession.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DateFirstProfession = "";
-  } else {
-    payload.DateFirstProfession = sp[0];
-  }
+	sp = payload.DateFirstVows.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateFirstVows = '';
+	} else {
+		payload.DateFirstVows = sp[0];
+	}
 
-  sp = payload.DateFirstVows.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DateFirstVows = "";
-  } else {
-    payload.DateFirstVows = sp[0];
-  }
+	sp = payload.DateNovitiate.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateNovitiate = '';
+	} else {
+		payload.DateNovitiate = sp[0];
+	}
 
-  sp = payload.DateNovitiate.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DateNovitiate = "";
-  } else {
-    payload.DateNovitiate = sp[0];
-  }
+	sp = payload.BirthDate.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.BirthDate = '';
+	} else {
+		payload.BirthDate = sp[0];
+	}
 
-  sp = payload.BirthDate.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.BirthDate = "";
-  } else {
-    payload.BirthDate = sp[0];
-  }
+	sp = payload.DateDeceased.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateDeceased = '';
+	} else {
+		payload.DateDeceased = sp[0];
+	}
 
-  sp = payload.DateDeceased.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DateDeceased = "";
-  } else {
-    payload.DateDeceased = sp[0];
-  }
+	sp = payload.DateRecordCreated.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateRecordCreated = '';
+	} else {
+		payload.DateRecordCreated = sp[0];
+	}
 
-  sp = payload.DateRecordCreated.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DateRecordCreated = "";
-  } else {
-    payload.DateRecordCreated = sp[0];
-  }
+	sp = payload.DateRemoved.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateRemoved = '';
+	} else {
+		payload.DateRemoved = sp[0];
+	}
 
-  sp = payload.DateRemoved.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DateRemoved = "";
-  } else {
-    payload.DateRemoved = sp[0];
-  }
-
-  console.log(payload);
-  return payload;
+	sp = payload.DateLifeVows.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateLifeVows = '';
+	} else {
+		payload.DateLifeVows = sp[0];
+	}
+	return payload;
 }
 
 export async function updateMember(id, FieldName, Value) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const dataArray = new FormData();
-  dataArray.append("field", FieldName);
-  dataArray.append("value", Value);
+	const dataArray = new FormData();
+	dataArray.append('field', FieldName);
+	dataArray.append('value', Value);
 
-  const request = {
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    body: dataArray,
-    headers: { Authorization: "Bearer " + jwt },
-  };
+	const request = {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		body: dataArray,
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
 
-  const response = await fetch(`${server}/api/v1/member/${id}`, request);
-  const payload = await response.json();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
+	const response = await fetch(`${server}/api/v1/member/${id}`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
 }
 
 export async function subsearch(query) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const dataArray = new FormData();
-  dataArray.append("query", query);
+	const dataArray = new FormData();
+	dataArray.append('query', query);
 
-  const request = {
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    body: dataArray,
-    headers: { Authorization: "Bearer " + jwt },
-  };
+	const request = {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		body: dataArray,
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
 
-  const response = await fetch(`${server}/api/v1/subsearch`, request);
-  const payload = await response.json();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
-  return payload;
+	const response = await fetch(`${server}/api/v1/subsearch`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
+	return payload;
 }
 
 export async function getSubscriber(id) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const request = {
-    method: "GET",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    headers: { Authorization: "Bearer " + jwt },
-  };
+	const request = {
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
 
-  const response = await fetch(`${server}/api/v1/subscriber/${id}`, request);
-  const payload = await response.json();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
+	const response = await fetch(`${server}/api/v1/subscriber/${id}`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
 
-  // trim unnecessary times
-  let sp = payload.DatePaid.split("T");
-  if (sp[0] == "0001-01-01") {
-    payload.DatePaid = "";
-  } else {
-    payload.DatePaid = sp[0];
-  }
+	// trim unnecessary times
+	let sp = payload.DatePaid.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DatePaid = '';
+	} else {
+		payload.DatePaid = sp[0];
+	}
 
-  return payload;
+	return payload;
 }
 
 export async function updateSubscriber(id, FieldName, Value) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const dataArray = new FormData();
-  dataArray.append("field", FieldName);
-  dataArray.append("value", Value);
+	const dataArray = new FormData();
+	dataArray.append('field', FieldName);
+	dataArray.append('value', Value);
 
-  const request = {
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    body: dataArray,
-    headers: { Authorization: "Bearer " + jwt },
-  };
+	const request = {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		body: dataArray,
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
 
-  const response = await fetch(`${server}/api/v1/member/${id}`, request);
-  const payload = await response.json();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
+	const response = await fetch(`${server}/api/v1/member/${id}`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
 }
 
 export async function report(reportname) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const request = {
-    method: "GET",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    headers: { Authorization: "Bearer " + jwt },
-  };
+	const request = {
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
 
-  const response = await fetch(
-    `${server}/api/v1/report/${reportname}`,
-    request
-  );
-  const payload = await response.text();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
+	const response = await fetch(`${server}/api/v1/report/${reportname}`, request);
+	const payload = await response.text();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
 
-  // https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
-  const blob = new Blob([payload], { type: "text/csv;charset=utf-8;" });
-  if (navigator.msSaveBlob) {
-    // IE 10+
-    navigator.msSaveBlob(blob, `${reportname}.csv`);
-  } else {
-    const link = document.createElement("a");
-    if (link.download !== undefined) {
-      // feature detection
-      // Browsers that support HTML5 download attribute
-      const url = URL.createObjectURL(blob);
-      link.setAttribute("href", url);
-      link.setAttribute("download", `${reportname}.csv`);
-      link.style.visibility = "hidden";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  }
+	// https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+	const blob = new Blob([payload], { type: 'text/csv;charset=utf-8;' });
+	if (navigator.msSaveBlob) {
+		// IE 10+
+		navigator.msSaveBlob(blob, `${reportname}.csv`);
+	} else {
+		const link = document.createElement('a');
+		if (link.download !== undefined) {
+			// feature detection
+			// Browsers that support HTML5 download attribute
+			const url = URL.createObjectURL(blob);
+			link.setAttribute('href', url);
+			link.setAttribute('download', `${reportname}.csv`);
+			link.style.visibility = 'hidden';
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		}
+	}
 }
 
 export async function createMember(firstname, lastname) {
-  const jwt = localStorage.getItem("jwt");
-  if (jwt === undefined || jwt === null) {
-    throw new Error("Not Logged in");
-  }
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
 
-  const dataArray = new FormData();
-  dataArray.append("firstname", firstname);
-  dataArray.append("lastname", lastname);
+	const dataArray = new FormData();
+	dataArray.append('firstname', firstname);
+	dataArray.append('lastname', lastname);
 
-  const request = {
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-    redirect: "manual",
-    referrerPolicy: "origin",
-    headers: { Authorization: "Bearer " + jwt },
-    body: dataArray,
-  };
+	const request = {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		headers: { Authorization: 'Bearer ' + jwt },
+		body: dataArray
+	};
 
-  const response = await fetch(
-    `${server}/api/v1/member`,
-    request
-  );
-  const payload = await response.json();
-  if (response.status != 200) {
-    console.log("server returned ", response.status);
-    throw new Error(payload.error);
-  }
-  console.log(payload);
+	const response = await fetch(`${server}/api/v1/member`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
+	return payload.id;
+}
 
-  return payload.id;
+export async function postRegister(email) {
+	const dataArray = new FormData();
+	dataArray.append('email', email);
+
+	const request = {
+		method: 'POST',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		body: dataArray
+	};
+
+	const response = await fetch(`${server}/api/v1/register`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
+	return true;
+}
+
+export async function getMeFromServer() {
+	const jwt = localStorage.getItem('jwt');
+	if (jwt === undefined || jwt === null) {
+		throw new Error('Not Logged in');
+	}
+	// const token = JSON.parse(window.atob(jwt.split(".")[1]).toString());
+
+	const request = {
+		method: 'GET',
+		mode: 'cors',
+		credentials: 'include',
+		redirect: 'manual',
+		referrerPolicy: 'origin',
+		headers: { Authorization: 'Bearer ' + jwt }
+	};
+
+	const response = await fetch(`${server}/api/v1/me`, request);
+	const payload = await response.json();
+	if (response.status != 200) {
+		console.log('server returned ', response.status);
+		throw new Error(payload.error);
+	}
+
+	// trim unnecessary times
+	let sp = payload.DateReaffirmation.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateReaffirmation = '';
+	} else {
+		payload.DateReaffirmation = sp[0];
+	}
+
+	sp = payload.DateFirstVows.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateFirstVows = '';
+	} else {
+		payload.DateFirstVows = sp[0];
+	}
+
+	sp = payload.DateNovitiate.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateNovitiate = '';
+	} else {
+		payload.DateNovitiate = sp[0];
+	}
+
+	sp = payload.BirthDate.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.BirthDate = '';
+	} else {
+		payload.BirthDate = sp[0];
+	}
+
+	sp = payload.DateDeceased.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateDeceased = '';
+	} else {
+		payload.DateDeceased = sp[0];
+	}
+
+	sp = payload.DateRecordCreated.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateRecordCreated = '';
+	} else {
+		payload.DateRecordCreated = sp[0];
+	}
+
+	sp = payload.DateRemoved.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateRemoved = '';
+	} else {
+		payload.DateRemoved = sp[0];
+	}
+
+	sp = payload.DateLifeVows.split('T');
+	if (sp[0] == '0001-01-01') {
+		payload.DateLifeVows = '';
+	} else {
+		payload.DateLifeVows = sp[0];
+	}
+	return payload;
+}
+
+export async function updateMe() {
+	throw new Error('editing your own data is not yet available');
 }
