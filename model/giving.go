b6b4fb9f5174945bb6d391562ef41ccc8d9063f6
Scot Bontrager <cloudkucooland@gmail.com>
2024-10-05 01:31:02 +0000

    add giving records

---
+++ model/giving.go
@@ -0,0 +1,67 @@
+package model
+
+import (
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/cloudkucooland/OSL-Online/email"
+)
+
+type GivingRecord struct {
+        EntryID            int
+	ID                 int
+	Amount             float64
+	Check              int
+	Transaction        int
+	Description        string
+	Date               time.Time
+}
+
+func (n GivingRecord) Store() error {
+	member, err := GetMember(n.ID, true)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	_, err = db.Exec("INSERT INTO `giving` (`entryID`, `id`, `amount`, `check`, `transaction`, `description`, `date`) VALUES (0,?,?,?,?,?,curdate())", n.ID, n.Amount, n.Check, n.Transaction, n.Description)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	if err := email.SendGiving(member.PrimaryEmail, member.OSLName(), fmt.Sprintf("%.2f", n.Amount), n.Description); err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (m *Member) GivingRecords() ([]GivingRecord, error) {
+	gr := make([]GivingRecord, 0)
+
+	rows, err := db.Query("SELECT `entryID`, `amount`, `check`, `transaction`, `description`, `date` FROM `giving` WHERE `id` = ? ORDER BY `date`", m.ID)
+	if err != nil && err == sql.ErrNoRows {
+		return gr, nil
+	}
+	if err != nil {
+		slog.Error(err.Error())
+		return gr, err
+	}
+
+	var g GivingRecord
+	var d string
+	for rows.Next() {
+		err := rows.Scan(&g.EntryID, &g.Amount, &g.Check, &g.Transaction, &g.Description, &d)
+		if err != nil {
+			slog.Error(err.Error())
+			continue
+		}
+		g.Date , _ = time.Parse(format, d)
+
+		gr = append(gr, g)
+	}
+	return gr, nil
+}
