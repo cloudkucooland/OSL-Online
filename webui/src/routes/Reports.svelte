11428a54562b34c26f7c5e2b77244d64dc75ed1d
Scot Bontrager <cloudkucooland@gmail.com>
2024-10-25 03:42:39 +0000

    cleanup

--- webui/src/routes/Reports.svelte
+++ webui/src/routes/Reports.svelte
@@ -7,37 +7,6 @@
 	if ($me === undefined) {
 		push('/Login');
 	}
-
-	async function notrenewed() {
-		report('notrenewed');
-	}
-
-	async function expired() {
-		report('expired');
-	}
-
-	async function email() {
-		report('email');
-	}
-
-	async function annual() {
-		report('annual');
-	}
-
-	async function life() {
-		report('life');
-	}
-
-	async function avery() {
-		report('avery');
-	}
-	async function doxprint() {
-		report('doxprint');
-	}
-
-	async function doxemail() {
-		report('doxemail');
-	}
 </script>
 
 <svelte:head>
@@ -45,13 +14,28 @@
 </svelte:head>
 
 <ul>
-	<li><a href="#/reports" on:click={notrenewed}>Not Renewed this year</a></li>
-	<li><a href="#/reports" on:click={expired}>"Active" but not renewed for 2 years or longer</a></li>
-	<li><a href="#/reports" on:click={email}>All Member Email List</a></li>
-	<li><a href="#/reports" on:click={annual}>Annual Vows Mail Merge</a></li>
-	<li><a href="#/reports" on:click={life}>Life Vows Mail Merge</a></li>
-	<li><a href="#/reports" on:click={avery}>Active member Avery Labels</a> (not working yet)</li>
-	<li><a href="#/reports" on:click={doxprint}>Doxology Mailing List (member + subscriber)</a></li>
-	<li><a href="#/reports" on:click={doxemail}>Doxology Email List (member + subscriber)</a></li>
-	<li><a href="#/reports" on:click={() => report('fontemail')}>Font Email List (for google groups update)</a></li>
+	<li><a href="#/reports" on:click={() => report('notrenewed')}>Not Renewed this year</a></li>
+	<li>
+		<a href="#/reports" on:click={() => report('expired')}
+			>"Active" but not renewed for 2 years or longer</a
+		>
+	</li>
+	<li><a href="#/reports" on:click={() => report('email')}>All Member Email List</a></li>
+	<li><a href="#/reports" on:click={() => report('annual')}>Annual Vows Mail Merge</a></li>
+	<li><a href="#/reports" on:click={() => report('life')}>Life Vows Mail Merge</a></li>
+	<li>
+		<a href="#/reports" on:click={() => report('doxprint')}
+			>Doxology Mailing List (member + subscriber)</a
+		>
+	</li>
+	<li>
+		<a href="#/reports" on:click={() => report('doxemail')}
+			>Doxology Email List (member + subscriber)</a
+		>
+	</li>
+	<li>
+		<a href="#/reports" on:click={() => report('fontemail')}
+			>Font Email List (for google groups update)</a
+		>
+	</li>
 </ul>
