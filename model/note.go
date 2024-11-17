8ff59ca0e83d4a2b70218330d762edc26ae678f5
Scot Bontrager <cloudkucooland@gmail.com>
2024-11-17 03:19:56 +0000

    feature request

---
+++ model/note.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"time"
+)
+
+type NoteID uint
+
+type Note struct {
+	ID NoteID
+	member MemberID
+	date time.Time
+	note string
+}
+
+func  (m MemberID) GetNotes() ([]Note, error) {
+	notes := make([]Note, 0)
+
+	// TODO XXX
+
+	return notes, nil
+}
+
+func (n Note) Store() error {
+	// TODO XXX
+
+	return nil
+}
+
+func (n NoteID) Delete() error {
+	// TODO XXX
+
+	return nil
+}
