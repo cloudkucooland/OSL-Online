2644ecc0792631065291325cb1b8871bda20b715
Scot Bontrager <cloudkucooland@gmail.com>
2025-09-18 22:11:41 +0000

    add some new files

---
+++ rest/notes.go
@@ -0,0 +1,117 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strconv"
+
+	"github.com/cloudkucooland/OSL-Online/model"
+	"github.com/julienschmidt/httprouter"
+)
+
+func getNotes(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	headers(w, r)
+
+	s := ps.ByName("id")
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	m := model.MemberID(id)
+	slog.Info("get notes", "member", id, "requester", getUser(r))
+	notes, err := m.GetNotes()
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(notes); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func postNote(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	headers(w, r)
+	if err := r.ParseMultipartForm(1024); err != nil {
+		slog.Warn(err.Error())
+		http.Error(w, jsonError(err), http.StatusNotAcceptable)
+		return
+	}
+
+	s := ps.ByName("id")
+	if s == "" {
+		err := fmt.Errorf("id not set")
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusNotAcceptable)
+		return
+	}
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusNotAcceptable)
+		return
+	}
+	m := model.MemberID(id)
+
+	notecontent := r.PostFormValue("note")
+	if notecontent  == "" {
+		err := fmt.Errorf("note not set")
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusNotAcceptable)
+		return
+	}
+
+	note := model.Note {
+		ID:    0,
+		Member: m,
+		Note: notecontent,
+	}
+	if err := note.Store(); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, jsonStatusOK)
+}
+
+func deleteNote(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	headers(w, r)
+	if err := r.ParseMultipartForm(1024); err != nil {
+		slog.Warn(err.Error())
+		http.Error(w, jsonError(err), http.StatusNotAcceptable)
+		return
+	}
+
+	s := ps.ByName("id")
+	if s == "" {
+		err := fmt.Errorf("id not set")
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusNotAcceptable)
+		return
+	}
+	id, err := strconv.Atoi(s)
+	if err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusNotAcceptable)
+		return
+	}
+	n := model.NoteID(id)
+
+	if err := n.Delete(); err != nil {
+		slog.Error(err.Error())
+		http.Error(w, jsonError(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, jsonStatusOK)
+}
+
---
+++ webui/src/routes/Necrology.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { Table } from '@flowbite-svelte-plugins/datatable';
+	import { getNecrology } from '../oo';
+
+	const { me } = getContext('oo');
+	if ($me === undefined) {
+		push('/Login');
+	}
+
+	$: items = [
+		{
+			'First Name': 'Loading',
+			'Last Name': 'Loading',
+			'Date Deceased': 'Loading',
+			Locality: 'Loading'
+		}
+	];
+
+	const opts = {
+		paging: true,
+		perPage: 100,
+		perPageSelect: [25, 50, 100],
+		sortable: true
+	};
+
+	// should not need the #await block, but we do...
+	async function getit() {
+		const w = [];
+		try {
+			const t = await getNecrology();
+			t.forEach((i) => {
+				const d = new Date(Date.parse(i.DateDeceased));
+				const dd = d.toLocaleDateString();
+
+				const j = {
+					'First Name': i.FirstName,
+					'Last Name': i.LastName,
+					'Date Deceased': dd,
+					Locality: i.Country + ': ' + i.State
+				};
+				w.push(j);
+			});
+			items = w;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>OSL Member Manager: Necrology</title>
+</svelte:head>
+
+{#await getit()}
+	... loading ....
+{:then { }}
+	<Table {items} dataTableOptions={opts} />
+{/await}
